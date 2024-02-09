3ad0cae4db9a57a4d54ebbba0247c0a8ad27f661
Tejas <tejasaggarwal10@gmail.com>
2024-02-09 10:18:03 +0530

    first-commit

---
+++ src/test/java/com/example/tejasgoyal/JDBC/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tejasgoyal.JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
