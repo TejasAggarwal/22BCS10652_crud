ce61d3bf0d8834ca4487ba471863d22df7e8e553
Tejas <tejasaggarwal10@gmail.com>
2024-02-09 10:28:40 +0530

    Final Commit

--- src/main/java/com/example/tejasgoyal/JDBC/ConnectionClass.java
+++ src/main/java/com/example/tejasgoyal/JDBC/ConnectionClass.java
@@ -1,5 +1,16 @@
 package com.example.tejasgoyal.JDBC;
 
-public class ConnectionClass {
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionClass {
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Connection con=null;
+		Class.forName("com.mysql.cj.jdbc.Driver");//Load and register driver
+		con=DriverManager.getConnection("jdbc:mysql://localhost:1208/jdbc_crud","tejas","Mysql@1208");
+		System.out.println("Connection Established");
+		return con;
+	}
 }
